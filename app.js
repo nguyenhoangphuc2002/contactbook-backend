ee4057d676a4aeeb7ce128e4561e3be6f17fd8c5
nguyenhoangphuc2002 <phucvuhaugiang@gmail.com>
2023-09-20 20:05:01 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const contactsRouter = require("./app/routes/contact.route");
 
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
     res.json({message: "Welcome to contack book application."});
 });
-
+app.use("/api/contacts", contactsRouter);
 module.exports = app;
