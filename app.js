0061e5800f020a2bcc377ee4d3f42bf4bd44f29f
nguyenhoangphuc2002 <phucvuhaugiang@gmail.com>
2023-10-04 22:44:46 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const contactsRouter = require("./app/routes/contact.route");
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
-    res.json({message: "Welcome to contack book application."});
+    res.json({message: "Welcome to contact book application."});
 });
 app.use("/api/contacts", contactsRouter);
 app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found"));
 });
-app.use((err, req, res, next) => {
+app.use((error, req, res, next) => {
     return res.status(error.statusCode || 500).json({
         message: error.message || "Internal Server Error",
     });
